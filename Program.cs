df3f5b1e010a3e1c6b87e3673831f0a6c424dfe9
Sweekriti Satpathy <swsat@microsoft.com>
2020-07-03 20:00:24 -0700

    updated code for Azure IoT Hub connection

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Device.Gpio;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
 
 namespace led_blink
 {
     class Program
     {
-        static void Main(string[] args)
+        //Azure IoT Hub setup
+        const string DeviceConnectionString = "<Your Azure IoT Hub Connection String>";
+        // Replace with the device id you used when you created the device in Azure IoT Hub
+        const string DeviceId = "<Your Device Id>";
+        static DeviceClient _deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString, TransportType.Mqtt);
+        static int _msgId = 0;
+        const int messageThreshold = 8;
+            
+        static async Task Main(string[] args)
         {
             // var pin = 17;
             var ledPin = 6;
@@ -31,14 +43,13 @@ namespace led_blink
 
                 while (true)
                 {
-                   
-
                     var buttonState = controller.Read(buttonPin);
                     Console.WriteLine("Button State" + buttonState);
                     Thread.Sleep(5000);
                     while(controller.Read(buttonPin) == PinValue.Low)
                     {
                         Console.WriteLine("Button Pressed!!");
+                        await SendMsgIotHub("Hello from .NET IoT! at " + DateTime.Now.ToShortTimeString() +" on " + DateTime.Now.ToShortDateString()) ;
                         Console.WriteLine($"Light for {lightTimeInMilliseconds}ms");
                         controller.Write(ledPin, PinValue.High);
                         Thread.Sleep(lightTimeInMilliseconds);
@@ -49,6 +60,31 @@ namespace led_blink
                     }
                 }
             }
+        }
+    
+        private static async Task SendMsgIotHub(string message)
+        {
+            var telemetry = new Telemetry() { MessageData=message, MessageId = _msgId++ };
+            string json = JsonConvert.SerializeObject(telemetry);
+            Console.WriteLine($"Sending {json}");
+            Message eventMessage = new Message(Encoding.UTF8.GetBytes(json));
+            eventMessage.Properties.Add("Action", "tweet");
+            if(_msgId > messageThreshold)
+            {
+                await _deviceClient.SendEventAsync(eventMessage).ConfigureAwait(false);
+                _msgId = 0;
+            }
+            else
+                Console.WriteLine(eventMessage);
+        }
+        class Telemetry
+        {
+            [JsonPropertyAttribute (PropertyName="messageData")] 
+            public string MessageData { get; set; } = "";
+            [JsonPropertyAttribute (PropertyName="messageId")] 
+            public int MessageId { get; set; } = 0;
+            [JsonPropertyAttribute (PropertyName="deviceId")] 
+            public string DeviceId {get; set;} = Program.DeviceId;
         }
     }
 }
